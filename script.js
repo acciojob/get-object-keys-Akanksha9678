8e3ce0ddab968696ce5a9de5e952e1a74e06b8fa
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-03-07 16:48:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let student={
+	name:"akanksha",
+	age:23,
+	city:"allahabad"
+}
+let arr=[];
+function getKeys(student){
+	let a=Object.Keys(student)
+	arr=[...a]
+	return arr
+}
